a966ccb3d705eb2fee537e0e9ee8f66da9f219be
Stefan Sakalos <stefan.sakalos@tipsport.cz>
2018-09-14 16:38:36 +0200

    add more dependencies to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email = 'author@gmail.com',
     description = 'Description of my package',
     packages = find_packages(),    
-    install_requires = ["matplotlib"],
+    install_requires = ["matplotlib", "IPython", "ipywidgets", "scipy", "pandas"],
 )
